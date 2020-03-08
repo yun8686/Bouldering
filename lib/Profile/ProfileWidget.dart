274add2c4ae5b6fcb041d63e7039d1de4f9946ff
yun8686 <yun.mes41@gmail.com>
2020-03-08 18:52:15 +0900

    add profile page

--- lib/Profile/ProfileWidget.dart
+++ lib/Profile/ProfileWidget.dart
@@ -1,29 +1,134 @@
-import 'package:bouldering_sns/GymDetail/Widgets/UserPanelWidget.dart';
+import 'package:bouldering_sns/CommonWidget/GymCard.dart';
 import 'package:flutter/material.dart';
 
-class ProfileWidget extends StatelessWidget {
+class ProfileWidget extends StatefulWidget {
+  @override
+  State<ProfileWidget> createState() => _ProfileWidgetState();
+
+}
+class _ProfileWidgetState extends State<ProfileWidget>
+    with SingleTickerProviderStateMixin {
+  List<String> _tabs = <String>["通知","自分の投稿"];
+  TabController _tabController;
+  @override
+  void initState() {
+//    _tabController = TabController(length: _tabs.length, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('プロフィール'),
+        title: const Text('マイページ'),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 10.0),
-            height: 240.0,
-            child: UserPanelWidget(),
+      body: DefaultTabController(
+        length: _tabs.length,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              // AppBarとTabBarの間のコンテンツ
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Container(height: 200.0, child: _HeaderCard()),
+                    _followInfoWidget(),
+                  ],
+                ),
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _StickyTabBarDelegate(
+                  TabBar(
+                    tabs: _tabs.map((String name) => Tab(text: name)).toList(),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return GymCard.create(
+                context,
+                onTap: () {
+                }
+              );
+            },
           ),
-          Text("自己紹介文"),
-          Text("登ったグレード"),
-          Text("地域"),
-          Text("SNS連携"),
-
-        ],
+        ),
       ),
+
     );
   }
+
+  Widget _HeaderCard(){
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          child: Image.network(
+            'https://placeimg.com/640/480/any',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            padding: EdgeInsets.only(left: 16),
+            height: 60,
+            child: Column(
+              children: <Widget>[
+                Align(alignment: Alignment.centerLeft,child: Text("ぼるこむ新宿店", style: TextStyle(color: Colors.white, fontSize: 24),),),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // フォロー一覧
+  Widget _followInfoWidget(){
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(8),
+          alignment: Alignment.centerLeft,
+          child: Text("フォロー 100人"),
+        ),
+        Container(
+          margin: EdgeInsets.all(8),
+          alignment: Alignment.centerLeft,
+          child: Text("フォロワー 100人"),
+        ),
+      ],
+    );    
+  }  
+
 }
+class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _StickyTabBarDelegate(this.tabBar);
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(color: Colors.blue, child: tabBar);
+  }
+
+  @override
+  bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
+  }
+}
+
 
 
